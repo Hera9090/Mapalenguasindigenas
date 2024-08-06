670519351b7ecfacc7c55417550d1b3915ae97bb
Anna <95587525+Hera9090@users.noreply.github.com>
2024-08-05 18:44:45 -0600

    Update JAVA-BATO-REP.js

--- JAVA-BATO-REP.js
+++ JAVA-BATO-REP.js
@@ -1,5 +1,5 @@
-var titulo = document.getElementById('title');
-var tablaDatos = document.getElementById('tablaDatos');
+let titulo = document.getElementById('title');
+let tablaDatos = document.getElementById('tablaDatos');
 
 title.addEventListener('mouseenter', function() {
     tablaDatos.style.display = 'table'; 
@@ -9,8 +9,8 @@ title.addEventListener('mouseleave', function() {
     tablaDatos.style.display = 'none'; 
 });
 
-var titulo = document.getElementById('image');
-var tablaDatos = document.getElementById('img-div');
+let titulo = document.getElementById('image');
+let tablaDatos = document.getElementById('img-div');
 
 image.addEventListener('mouseenter', function() {
     tablaDatos.style.display = 'figure'; 
